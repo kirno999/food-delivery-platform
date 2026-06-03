c7c31ab801779c59d96c5d04ef08a474e906426d
kirno999 <3547910499@qq.com>
2026-06-03 16:21:20 +0800

    添加 Spring Boot 主应用程序入口

---
+++ src/main/java/com/edu/sdu/Application.java
@@ -0,0 +1,15 @@
+package com.edu.sdu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot 主应用程序入口
+ */
+@SpringBootApplication
+public class Application {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
